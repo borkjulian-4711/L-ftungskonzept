d4225edde0aee9c8a5f5b5f8fa5acbc20cf16c12
borkjulian-4711 <bork.julian@googlemail.com>
2026-04-15 11:33:27 +0200

    Update din18017.py

--- logic/din18017.py
+++ logic/din18017.py
@@ -1,8 +1,17 @@
-def get_abluft(room_type):
-    mapping = {
+def get_abluft(room_type, category):
+
+    base = {
         "Bad": 40,
         "WC": 25,
         "Küche": 60,
         "Sonstiger Abluftraum": 30
-    }
-    return mapping.get(room_type, 0)
+    }.get(room_type, 0)
+
+    factor = {
+        "R-ZD": 1.0,
+        "R-BD": 0.8,
+        "R-PN": 1.2,
+        "R-PD": 1.0
+    }.get(category, 1.0)
+
+    return base * factor
