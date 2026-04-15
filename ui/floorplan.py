ea69c27646e943ce38e3597ab646f2aa0212e8ab
borkjulian-4711 <bork.julian@googlemail.com>
2026-04-15 12:14:15 +0200

    Update floorplan.py

--- ui/floorplan.py
+++ ui/floorplan.py
@@ -3,32 +3,28 @@ from PIL import Image
 
 def upload_floorplan():
 
-    uploaded_file = st.file_uploader("Grundriss hochladen", type=["png","jpg","jpeg"])
+    file = st.file_uploader("Grundriss hochladen", type=["png","jpg"])
 
-    if uploaded_file:
-        image = Image.open(uploaded_file)
-        st.image(image, use_container_width=True)
-        return image
+    if file:
+        img = Image.open(file)
+        st.image(img)
+        return img
 
     return None
 
 
 def define_rooms():
 
-    st.subheader("Räume im Grundriss definieren")
-
     if "rooms_pos" not in st.session_state:
         st.session_state["rooms_pos"] = []
 
-    room_name = st.text_input("Raumname")
-
-    x = st.number_input("X-Position", 0, 2000, 100)
-    y = st.number_input("Y-Position", 0, 2000, 100)
+    name = st.text_input("Raumname")
+    x = st.number_input("X", 0, 2000, 100)
+    y = st.number_input("Y", 0, 2000, 100)
 
     if st.button("Raum hinzufügen"):
-
         st.session_state["rooms_pos"].append({
-            "name": room_name,
+            "name": name,
             "x": x,
             "y": y
         })
