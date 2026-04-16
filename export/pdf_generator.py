237d5aa24518be335afd83319a1ad5c2b73b7e4a
borkjulian-4711 <bork.julian@googlemail.com>
2026-04-16 07:50:10 +0200

    Update pdf_generator.py

--- export/pdf_generator.py
+++ export/pdf_generator.py
@@ -5,13 +5,11 @@ from reportlab.lib.styles import getSampleStyleSheet
 
 styles = getSampleStyleSheet()
 
-
 def table_style():
     return [
         ("GRID", (0,0), (-1,-1), 0.8, colors.black),
         ("FONTSIZE", (0,0), (-1,-1), 8),
     ]
-
 
 def create_multi_pdf(file, project):
 
@@ -23,9 +21,16 @@ def create_multi_pdf(file, project):
         meta = data["meta"]
         res = data["res"]
 
-        elements.append(Paragraph("Lüftungskonzept DIN 1946-6", styles["Title"]))
+        elements.append(Paragraph("Formblatt A", styles["Heading2"]))
 
-        # Lüftungsstufen
+        elements.append(Table([
+            ["Lüftungskonzept erforderlich",
+             "Ja" if res["formblatt_a"]["erforderlich"] else "Nein"],
+            ["Begründung", res["formblatt_a"]["begruendung"]],
+        ], style=table_style()))
+
+        elements.append(Spacer(1,10))
+
         elements.append(Paragraph("Lüftungsstufen", styles["Heading3"]))
         elements.append(Table([
             ["FL", res["levels"]["FL"]],
@@ -33,24 +38,6 @@ def create_multi_pdf(file, project):
             ["NL", res["levels"]["NL"]],
             ["IL", res["levels"]["IL"]],
         ], style=table_style()))
-
-        elements.append(Spacer(1,10))
-
-        # Räume
-        elements.append(Paragraph("Räume", styles["Heading3"]))
-
-        table_data = [["Raum","Typ","Abluft"]]
-
-        for _, r in res["df"].iterrows():
-            table_data.append([r["Raum"], r["Typ"], r.get("Abluft (m³/h)", "")])
-
-        elements.append(Table(table_data, style=table_style()))
-
-        elements.append(Spacer(1,10))
-
-        # Text
-        elements.append(Paragraph("Beschreibung", styles["Heading3"]))
-        elements.append(Paragraph(res["text"], styles["Normal"]))
 
         elements.append(PageBreak())
 
