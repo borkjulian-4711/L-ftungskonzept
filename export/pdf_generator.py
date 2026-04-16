6018f00b836d0ac711cf48fef9dae166481d07c3
borkjulian-4711 <bork.julian@googlemail.com>
2026-04-16 07:57:12 +0200

    Update pdf_generator.py

--- export/pdf_generator.py
+++ export/pdf_generator.py
@@ -13,18 +13,22 @@ def create_multi_pdf(file, project):
 
         res = data["res"]
 
-        # Formblatt D
-        elements.append(Paragraph("Formblatt D – Maßnahmen", styles["Heading2"]))
+        elements.append(Paragraph("Lüftungskonzept DIN 1946-6", styles["Title"]))
 
-        elements.append(Paragraph(
-            f"Maßnahme: {res['formblatt_d']['massnahme']}",
-            styles["Normal"]
-        ))
+        elements.append(Spacer(1,10))
 
-        elements.append(Paragraph(
-            f"Begründung: {res['formblatt_d']['begruendung']}",
-            styles["Normal"]
-        ))
+        # A–D kurz darstellen
+        elements.append(Paragraph("Formblatt A–D", styles["Heading2"]))
+        elements.append(Paragraph(str(res["formblatt_a"]), styles["Normal"]))
+        elements.append(Paragraph(str(res["formblatt_b"]), styles["Normal"]))
+        elements.append(Paragraph(str(res["formblatt_c"]), styles["Normal"]))
+        elements.append(Paragraph(str(res["formblatt_d"]), styles["Normal"]))
+
+        elements.append(Spacer(1,10))
+
+        # Formblatt E
+        elements.append(Paragraph("Formblatt E – Ergebnis", styles["Heading2"]))
+        elements.append(Paragraph(res["formblatt_e"], styles["Normal"]))
 
         elements.append(PageBreak())
 
