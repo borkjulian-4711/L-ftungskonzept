1fa6cafb45a69989995428a289f9e6dad7392a11
borkjulian-4711 <bork.julian@googlemail.com>
2026-04-16 11:41:36 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+FIRMA = {
+    "name": "PGB",
+    "anschrift": "Ludwigsburger Straße 18\n50739 Köln",
+    "kontakt": "Tel: 01234 56789\nMail: info@pg-bauphysik.de",
+    "logo_path": "assets/logo.png"
+}
